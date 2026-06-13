ed6dabc3fa484eb080d62d5e3fbc56f55c5bc0dd
rafi0012 <rafiultalukdar007@gmail.com>
2026-06-13 19:44:52 +0600

    Add another

---
+++ Chapter03/arithmetic-operators02.java
@@ -0,0 +1,27 @@
+public class Arithmetic {
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 5;
+        int result;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        result = a + b;
+        System.out.println("a + b = " + result);
+
+        result = a - b;
+        System.out.println("a - b = " + result);
+
+        result = a * b;
+        System.out.println("a * b = " + result);
+
+        result = a / b;
+        System.out.println("a / b = " + result);
+
+        result = a % b;
+        System.out.println("a % b = " + result);
+    }
+}
