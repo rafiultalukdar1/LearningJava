15fa80478a9f856388909bee3ada9ab8d54309f4
rafi0012 <rafiultalukdar007@gmail.com>
2026-06-15 19:41:21 +0600

    Add another

---
+++ Chapter03/arithmetic-operators04.java
@@ -0,0 +1,15 @@
+public class Arithmetic {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println("a = " + a);
+
+        a++;
+        System.out.println("After a++ = " + a);
+
+        a--;
+        System.out.println("After a-- = " + a);
+    }
+}
