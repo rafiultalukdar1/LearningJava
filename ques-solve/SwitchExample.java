ad1232c0bbcb26dfdffa9696f0e8c6c936934281
rafi0012 <rafiultalukdar007@gmail.com>
2026-06-23 21:41:40 +0600

    Add another example

---
+++ ques-solve/SwitchExample.java
@@ -0,0 +1,25 @@
+public class SwitchExample {
+
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+
+            case 1:
+                System.out.println("Saturday");
+                break;
+
+            case 2:
+                System.out.println("Sunday");
+                break;
+
+            case 3:
+                System.out.println("Monday");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+}
