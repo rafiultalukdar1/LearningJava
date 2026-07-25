c4a67cdf6e0eb5aeccb231e8c82c461c2e29e3be
rafi0012 <rafiultalukdar007@gmail.com>
2026-07-25 11:56:13 +0600

    Explore TestArray

---
+++ Chapter05/TestArray.java
@@ -0,0 +1,32 @@
+public class TestArray {
+
+    public static void main(String[] args) {
+
+        double[] myList = {1.9, 2.9, 3.4, 3.5};
+
+        // Print all elements
+        for (int i = 0; i < myList.length; i++) {
+            System.out.println(myList[i] + " ");
+        }
+
+        // Sum all elements
+        double total = 0;
+
+        for (int i = 0; i < myList.length; i++) {
+            total += myList[i];
+        }
+
+        System.out.println("Total is " + total);
+
+        // Find largest element
+        double max = myList[0];
+
+        for (int i = 1; i < myList.length; i++) {
+            if (myList[i] > max) {
+                max = myList[i];
+            }
+        }
+
+        System.out.println("Max is " + max);
+    }
+}
