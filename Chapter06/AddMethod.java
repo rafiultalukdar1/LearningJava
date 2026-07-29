f3c4aa39dc9a5121f3e6e7f0b6f9953962c8948c
rafi0012 <rafiultalukdar007@gmail.com>
2026-07-29 10:21:05 +0600

    Explore AddMethod

---
+++ Chapter06/AddMethod.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AddMethod {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int num1, num2, addition;
+
+        System.out.print("Enter 1st number: ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter 2nd number: ");
+        num2 = input.nextInt();
+
+        addition = add(num1, num2);
+
+        System.out.println("Addition is: " + addition);
+    }
+
+    public static int add(int num1, int num2) {
+
+        int result = num1 + num2;
+
+        return result;
+    }
+}
